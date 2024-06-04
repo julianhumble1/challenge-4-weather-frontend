cc1dc8a3ade583452901b9a4371d75e91cd1f346
Julian Humble <humblejulian@gmail.com>
2024-06-04 09:13:32 +0100

    Installed bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 const App = () => {
-    return <></>;
+    return (
+    <>
+            <h1 className="color-danger">Hello World</h1>
+            <button type="button" class="btn btn-primary">Primary</button>
+    </>
+    );
 };
 
 export default App;
