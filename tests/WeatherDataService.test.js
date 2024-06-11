40a1dab3928fd7b2722f3634a71f5d7a49115fd6
Julian Humble <humblejulian@gmail.com>
2024-06-11 21:05:57 +0100

    WeatherDataService Test 4&5 passed

--- tests/WeatherDataService.test.js
+++ tests/WeatherDataService.test.js
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom/vitest";
 
 import WeatherDataService from "../src/services/weatherDataService.js";
+import { describe } from "vitest";
 
 describe("WeatherDataService class tests", () => {
     describe("getLocationDataByName tests", () => {
@@ -32,6 +33,28 @@ describe("WeatherDataService class tests", () => {
             const responseCode = response.status;
             // Assert
             expect(responseCode).toBe(404);
+        })
+    })
+
+    describe("getLocationDataById tests", async () => {
+        it("should return response status code 200 when valid term entered", async () => {
+            // Arrange
+            const validCityId = "2643743";
+            // Act 
+            const response = await WeatherDataService.getLocationDataById(validCityId);
+            const responseCode = response.status;
+            // Assert
+            expect(responseCode).toBe(200);
+        })
+
+        it("should return response with expected city name with valid term entered", async () => {
+            // Arrange
+            const validCityId = "2643743";
+            // Act 
+            const response = await WeatherDataService.getLocationDataById(validCityId);
+            const responseCity = response.data.city.name
+            // Assert
+            expect(responseCity).toBe("London");
         })
     })
 })
