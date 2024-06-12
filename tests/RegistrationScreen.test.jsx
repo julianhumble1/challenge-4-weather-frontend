e3c7deead9b3f7bec25d3dba08268a66d1802be0
Julian Humble <humblejulian@gmail.com>
2024-06-12 17:24:03 +0100

    Registration Screen test 1 passed

--- tests/RegistrationScreen.test.jsx
+++ tests/RegistrationScreen.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
-import { describe, it, expect } from "vitest"
+
 
 import RegistrationScreen from "../src/components/RegistrationScreen/RegistrationScreen.jsx"
 
