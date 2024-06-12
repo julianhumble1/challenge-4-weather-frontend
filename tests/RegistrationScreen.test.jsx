754575853864e834e671fa3fcc1d693d0e172bbb
Julian Humble <humblejulian@gmail.com>
2024-06-12 17:28:06 +0100

    Registration Screen test 2 passed

--- tests/RegistrationScreen.test.jsx
+++ tests/RegistrationScreen.test.jsx
@@ -3,6 +3,7 @@ import userEvent from "@testing-library/user-event"
 
 
 import RegistrationScreen from "../src/components/RegistrationScreen/RegistrationScreen.jsx"
+import { it } from "vitest"
 
 describe("Registration Screen tests", () => {
     describe("Email and password error message tests", () => {
@@ -15,6 +16,17 @@ describe("Registration Screen tests", () => {
             await userEvent.tab()
             // Assert
             expect(screen.getByText("Please enter a valid email address")).toBeInTheDocument();
+        })
+
+        it("should render invalid password message after entering an invalid passowrd", async () => {
+            // Arrange
+            render(<RegistrationScreen />) 
+            // Act
+            const passwordInput = await screen.findByPlaceholderText("Password")
+            await userEvent.type(passwordInput, "badpassword");
+            await userEvent.tab()
+            // Assert
+            expect(screen.getByText("Password must contain a special character, number and be at least 8 characters long")).toBeInTheDocument();
         })
     })
 })
